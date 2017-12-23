18af3c222832a9c68e8619c3ab9b874e4eee81fc
Travis Bennett <tandrewbennett@hotmail.com>
2017-12-22 22:24:50 -0600

    A mapper should not pluck results.

--- src/Builder.php
+++ src/Builder.php
@@ -613,31 +613,6 @@ class Builder
     }
 
     /**
-     * Get an array with the values of a given column.
-     *
-     * @param  string  $column
-     * @param  string|null  $key
-     * @return \Illuminate\Support\Collection
-     */
-    public function pluck($column, $key = null)
-    {
-        $results = $this->toBase()->pluck($column, $key);
-
-        // If the model has a mutator for the requested column, we will spin through
-        // the results and mutate the values so that the mutated version of these
-        // columns are returned as you would expect from these Eloquent models.
-        if (! $this->model->hasGetMutator($column) &&
-            ! $this->model->hasCast($column) &&
-            ! in_array($column, $this->model->getDates())) {
-            return $results;
-        }
-
-        return $results->map(function ($value) use ($column) {
-            return $this->model->newFromBuilder([$column => $value])->{$column};
-        });
-    }
-
-    /**
      * Paginate the given query.
      *
      * @param  int  $perPage
