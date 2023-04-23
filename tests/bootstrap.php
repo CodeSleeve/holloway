8e499b3fdf06412e729e865ff1da0149e8c93c32
Travis Bennett <tandrewbennett@hotmail.com>
2023-04-23 13:23:54 -0500

    Split mysql and pgsql config out, bind db.schema to fix breaks with Illuminate 9.x and 10.x packages.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,23 +13,45 @@ date_default_timezone_set('UTC');
 $loader = require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/illuminate/support/helpers.php';
 
+// Setup the container and the capsule
 $container = new Container;
 Facade::setFacadeApplication($container);
 $capsule = new Capsule($container);
+
+// Bind the db connection to the container
 $container['db'] = $capsule->getDatabaseManager();
 
+// Bind the db.schema builder to the container
+$container->bind('db.schema', function ($app) {
+    return $app['db']->connection()->getSchemaBuilder();
+});
+
+// Setup a postgres connection
 $capsule->addConnection([
-    //'driver'    => 'pgsql',
-    'driver'    => 'mysql',
+    'driver'    => 'pgsql',
     'host'      => 'localhost',
-    'database'  => 'holloway',
-    //'username'  => 'postgres',
-    'username'  => 'root',
+    'database'  => 'holloway_test',
+    'username'  => 'postgres',
     'password'  => 'password',
     'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => 'prefer',
 ]);
+
+// set up a mysql connection
+// $capsule->addConnection([
+//     'driver'    => 'mysql',
+//     'host'      => 'localhost',
+//     'database'  => 'holloway_test',
+//     'username'  => 'root',
+//     'password'  => 'password',
+//     'charset'   => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => '',
+//     'schema'    => 'public',
+// ]);
 
 // Make this Capsule instance available globally via static methods
 $capsule->setAsGlobal();
